bd1afd354542091819ed2cec3d24e4d5fc04e991
Adiney Ferreira <adiney.ferreira@gmail.com>
2018-01-05 15:35:51 -0500

    Changed validation output

--- src/main/java/com/kerberus/model/syntaxValidator/SyntaxAnalizer.java
+++ src/main/java/com/kerberus/model/syntaxValidator/SyntaxAnalizer.java
@@ -3,19 +3,10 @@ package com.kerberus.model.syntaxValidator;
 import java.io.BufferedReader;
 import java.io.StringReader;
 import java.util.LinkedList;
-import java.util.Locale.Category;
 
-import com.kerberus.model.syntaxValidator.rules.sql.*;
-import com.kerberus.model.syntaxValidator.rules.util.Categories;
-import com.kerberus.model.syntaxValidator.rules.util.CupUtil;
-import com.kerberus.model.syntaxValidator.rules.util.FlexUtil;
+import com.kerberus.model.syntaxValidator.rules.sql.Cup_Rules_SQL;
+import com.kerberus.model.syntaxValidator.rules.sql.JFlex_Rules_SQL;
 import com.kerberus.model.syntaxValidator.rules.util.PrettyStatement;
-import com.kerberus.model.syntaxValidator.rules.util.UtilFiles;
-
-/* DB Connection */
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class SyntaxAnalizer {
 	
@@ -33,7 +24,7 @@ public class SyntaxAnalizer {
 		
 		StringBuilder sb = new StringBuilder();
 		
-		sb.append("----- Lexical and Syntactical Analysis ------\n");
+		sb.append("----- Lexical and Syntactical Analysis -----\n");
 		//sb.append("Statement: \"" + sqlStatement + "\"\n");
 		
 		try {
@@ -66,12 +57,12 @@ public class SyntaxAnalizer {
 				sb.append("\n -------> Statement's syntax is All Good!" );
 			}
 			
-			sb.append("\n\n-------------- Findings Analysis -----------------------");
+			sb.append("\n\n-------------- Findings Analysis --------------");
 			sb.append("\n" + Syntax_Analyzer.Tables.size()+ " Tables found (to be validated):\n" + Syntax_Analyzer.Tables);
 			sb.append("\n" + Syntax_Analyzer.Columns.size()+ " Columns found (to be validated):\n" + Syntax_Analyzer.Columns);
 			
-			sb.append("\n\n-------------- SQL Statement Copy -----------------------\n");
-			
+			sb.append("\n\n-------------- SQL Statement Copy --------------\n");
+			sb.append(sqlStatement);
 			/*
 			for (int i = 0; i < Lex_Analyzer.StmtCopy.size(); i++) {
 				sb.append(Lex_Analyzer.StmtCopy.get(i));
